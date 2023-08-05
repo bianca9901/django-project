cce3b862f7adf625ada7a4f72ec274e2443f2c40
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-05 16:28:48 +0000

    Add user and event models

--- app_reservation/admin.py
+++ app_reservation/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import restaurant_event
+from .models import restaurant_user
 
-# Register your models here.
+admin.site.register(restaurant_event)
+admin.site.register(restaurant_user)
--- app_reservation/models.py
+++ app_reservation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class restaurant_user(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField('User Email')
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class restaurant_event(models.Model):
+    name = models.CharField('Event Name', max_length=100)
+    event_date = models.DateTimeField('Event Date')
+    description = models.TextField(blank=True)
+    attendees = models.ManyToManyField(restaurant_user, blank=True)
+
+    def __str__(self):
+        return self.name
