d46462ef0cc281684a1e9e9b9b7288f30bd48cee
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-05 14:30:03 +0000

    Set up the reservation apps dynamic calendar

--- app_reservation/views.py
+++ app_reservation/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
-# Create your views here.
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = 'John'
+    month = month.capitalize()
+    # Convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+
+    # Create a calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    #Get current year
+    now = datetime.now()
+    current_year = now.year
+    # Get current time
+    time = now.strftime('%H:%M %p')
+
+    return render(request, 'app_reservation/home.html', {'name': name, 'year': year,
+    'month': month, 'month_number': month_number, 'cal': cal, 'current_year': current_year, 'time': time, })
