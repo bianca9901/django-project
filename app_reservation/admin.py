a19634532c0c605ec0c24a6dd5fa277e492e9a07
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-06 08:51:41 +0000

    Modify admin panel

--- app_reservation/admin.py
+++ app_reservation/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import restaurant_event
 from .models import restaurant_user
 
-admin.site.register(restaurant_event)
+#admin.site.register(restaurant_event)
 admin.site.register(restaurant_user)
 
+@admin.register(restaurant_event)
+class EventAdmin(admin.ModelAdmin):
+    fields = ('name', 'event_date', 'description', 'employee')
+    list_display = ('name', 'event_date')
+    list_filter = ('event_date',)
+    ordering = ('-event_date',)
