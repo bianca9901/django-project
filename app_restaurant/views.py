a184d4829e78c8e4689a59c392b6f1f5425c5513
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-07 19:35:50 +0000

    Import model and form for reservation function to use

--- app_restaurant/views.py
+++ app_restaurant/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
-from .models import restaurant_event
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import restaurant_event, restaurant_reservation
+from .forms import ReservationForm
 
 #Homepage
 def home(request):
     return render(request, 'app_restaurant/home.html', {})
+
 
 #Events page
 def all_events(request): 
     event_list = restaurant_event.objects.all().order_by('event_date')
     return render(request, 'app_restaurant/restaurant_event_list.html',
     {'event_list': event_list})
+
+# Start building reservation logic
+def make_reservation(request, event_id):
+    selected_event = get_object_or_404()
 
 
