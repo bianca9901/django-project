a7534a58ae48465ab8cb8f4ba846d899317ab8c5
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-07 05:24:53 +0000

    Add comments to explain form fields styling in forms.py

--- members/forms.py
+++ members/forms.py
@@ -2,8 +2,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-# Extending Django standard forms by adding extra fields here.
-# Standard Django forms can not be styled in html. Widgets makes bootstrap styling possible ('form-control' is a bootstrap class).
+# Extending Django built-in authentication form by adding extra fields.
+# Extended Django fields styled. Widgets makes bootstrap styling possible ('form-control' is a bootstrap class).
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -12,3 +12,10 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+# Built-in Django authentication fields styled.
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
