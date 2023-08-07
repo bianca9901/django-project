cc7a805f9bff2d0fc6402a7669a977270ddaf03b
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-07 05:03:13 +0000

    Add styling to register form

--- members/forms.py
+++ members/forms.py
@@ -2,10 +2,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+# Extending Django standard forms by adding extra fields here.
+# Standard Django forms can not be styled in html. Widgets makes bootstrap styling possible ('form-control' is a bootstrap class).
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
