24f08cbc626fca3cb7f46b5a9f3ebf3ffe4d4c7f
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-21 15:53:20 +0000

    Fix too long lines and add comments

--- members/views.py
+++ members/views.py
@@ -4,7 +4,11 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterUserForm
 
+
 def login_user(request):
+    """
+    Handles user login
+    """
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -13,18 +17,27 @@ def login_user(request):
             login(request, user)
             return redirect('home')
         else:
-            messages.info(request, ('Invalid username or password. Please try again.'))
+            messages.info(request, (
+                'Invalid username or password. Please try again.'))
             return redirect('login')
 
     else:
         return render(request, 'authenticate/login.html', {})
 
+
 def logout_user(request):
+    """
+    Handles user logout
+    """
     logout(request)
     messages.success(request, ('You have been logged out.'))
     return redirect('home')
 
+
 def register_user(request):
+    """
+    Handles user registration
+    """
     if request.method == 'POST':
         form = RegisterUserForm(request.POST)
         if form.is_valid():
@@ -33,8 +46,10 @@ def register_user(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, ('Registration successfull! You are now logged in.'))
+            messages.success(request, (
+                'Registration successfull! You are now logged in.'))
             return redirect('home')
     else:
         form = RegisterUserForm()
-    return render(request, 'authenticate/register_user.html', {'form':form,})
+    return render(request, 'authenticate/register_user.html', {'form': form, })
+
