68c3294d50c01479f05aa59697d993d0be08531e
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-21 16:16:39 +0000

    Add comments and double blank lines to form classes

--- app_restaurant/forms.py
+++ app_restaurant/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from .models import restaurant_reservation, review
 
+
+# Reservation form
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = restaurant_reservation
         fields = ['number_of_friends']
 
 
+
+# Review form
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = review
