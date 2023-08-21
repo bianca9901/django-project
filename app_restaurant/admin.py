0dc705c07419d4b9ac5ec4df7bd9d44b7261c1c7
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-21 16:13:59 +0000

    Add comments to admin classes

--- app_restaurant/admin.py
+++ app_restaurant/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from .models import restaurant_event, restaurant_reservation, menu, review
 
 
+# Inline display for event reservations
 class ReservationInline(admin.TabularInline):
     model = restaurant_reservation
     extra = 0
 
 
+# Registering the restaurant_event model
 @admin.register(restaurant_event)
 class EventAdmin(admin.ModelAdmin):
     fields = ('name', 'event_date', 'available_spots', 'description', 'image')
@@ -16,6 +18,7 @@ class EventAdmin(admin.ModelAdmin):
     inlines = [ReservationInline]
 
 
+# Registering the menu model
 @admin.register(menu)
 class MenuAdmin(admin.ModelAdmin):
     fields = ('name', 'price', 'description', 'category')
@@ -23,6 +26,7 @@ class MenuAdmin(admin.ModelAdmin):
     list_filter = ('category',)
 
 
+# Registering the review model
 @admin.register(review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('user', 'review_text', 'pub_date')
