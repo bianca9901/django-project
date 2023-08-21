ede6e88230c6c7148f147fd2f4bfc90f863cc66f
bianca9901 teresa <biancawikberg99@yahoo.com>
2023-08-21 16:04:19 +0000

    Fix too long lines of code

--- members/forms.py
+++ members/forms.py
@@ -2,16 +2,29 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+
 # Extending Django built-in authentication form by adding extra fields.
-# Extended Django fields styled. Widgets makes bootstrap styling possible ('form-control' is a bootstrap class).
+# Extended Django fields styled. Widgets makes bootstrap styling possible
+# ('form-control' is a bootstrap class).
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control'}))
+    first_name = forms.CharField(
+        max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2'
+        )
+
 
 # Built-in Django authentication fields styled.
     def __init__(self, *args, **kwargs):
